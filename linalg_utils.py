7bb559292c5707a9e51141f2c92238b48c297015
James Winans <jpwinans@gmail.com>
2024-04-15 14:34:07 -0400

    updating style

--- linalg_utils.py
+++ linalg_utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 from sympy import symbols, Matrix, sympify
 
+
 def swap_rows(M, row_index_1, row_index_2):
     """
     Swap rows in the given matrix.
@@ -10,14 +11,16 @@ def swap_rows(M, row_index_1, row_index_2):
     - row_index_1 (int): Index of the first row to be swapped.
     - row_index_2 (int): Index of the second row to be swapped.
     """
- 
+
     M = M.copy()
     M[[row_index_1, row_index_2]] = M[[row_index_2, row_index_1]]
     return M
 
+
 def get_index_first_non_zero_value_from_column(M, column, starting_row):
     """
-    Retrieve the index of the first non-zero value in a specified column of the given matrix.
+    Retrieve the index of the first non-zero value in a specified column of
+    the given matrix.
 
     Parameters:
     - M (numpy.array): The input matrix to search for non-zero values.
@@ -25,25 +28,29 @@ def get_index_first_non_zero_value_from_column(M, column, starting_row):
     - starting_row (int): The starting row index for the search.
 
     Returns:
-    int: The index of the first non-zero value in the specified column, starting from the given row.
+    int: The index of the first non-zero value in the specified column,
+    starting from the given row.
                 Returns -1 if no non-zero value is found.
     """
-    column_array = M[starting_row:,column]
+    column_array = M[starting_row:, column]
     for i, val in enumerate(column_array):
-        if not np.isclose(val, 0, atol = 1e-5):
+        if not np.isclose(val, 0, atol=1e-5):
             index = i + starting_row
             return index
     return -1
 
-def get_index_first_non_zero_value_from_row(M, row, augmented = False):
+
+def get_index_first_non_zero_value_from_row(M, row, augmented=False):
     """
-    Find the index of the first non-zero value in the specified row of the given matrix.
+    Find the index of the first non-zero value in the specified row of the
+    given matrix.
 
     Parameters:
     - M (numpy.array): The input matrix to search for non-zero values.
     - row (int): The index of the row to search.
-    - augmented (bool): Pass this True if you are dealing with an augmented matrix, 
-                        so it will ignore the constant values (the last column in the augmented matrix).
+    - augmented (bool): Pass this True if you are dealing with an augmented
+        matrix, so it will ignore the constant values (the last column
+        in the augmented matrix).
 
     Returns:
     int: The index of the first non-zero value in the specified row.
@@ -52,12 +59,13 @@ def get_index_first_non_zero_value_from_row(M, row, augmented = False):
 
     M = M.copy()
     if augmented == True:
-        M = M[:,:-1]
+        M = M[:, :-1]
     row_array = M[row]
     for i, val in enumerate(row_array):
-        if not np.isclose(val, 0, atol = 1e-5):
+        if not np.isclose(val, 0, atol=1e-5):
             return i
     return -1
+
 
 def augmented_matrix(A, B):
     """
@@ -70,13 +78,15 @@ def augmented_matrix(A, B):
     Returns:
     - numpy.array: Augmented matrix obtained by horizontally stacking A and B.
     """
-    augmented_M = np.hstack((A,B))
+    augmented_M = np.hstack((A, B))
     return augmented_M
+
 
 def row_echelon_form(A, B):
     """
-    Utilizes elementary row operations to transform a given set of matrices, 
-    which represent the coefficients and constant terms of a linear system, into row echelon form.
+    Utilizes elementary row operations to transform a given set of matrices,
+    which represent the coefficients and constant terms of a linear system,
+    into row echelon form.
 
     Parameters:
     - A (numpy.array): The input square matrix of coefficients.
@@ -85,79 +95,90 @@ def row_echelon_form(A, B):
     Returns:
     numpy.array: A new augmented matrix in row echelon form with pivots as 1.
     """
-    
-    # Before any computation, check if matrix A (coefficient matrix) has non-zero determinant. 
+
+    # Before any computation, check if matrix A (coefficient matrix) has non-
+    # zero determinant.
     # It will be used the numpy sub library np.linalg to compute it.
 
     det_A = np.linalg.det(A)
 
     # Returns "Singular system" if determinant is zero
     if np.isclose(det_A, 0) == True:
-        return 'Singular system'
+        return "Singular system"
 
     # Make copies of the input matrices to avoid modifying the originals
     A = A.copy()
     B = B.copy()
 
     # Convert matrices to float to prevent integer division
-    A = A.astype('float64')
-    B = B.astype('float64')
+    A = A.astype("float64")
+    B = B.astype("float64")
 
     # Number of rows in the coefficient matrix
-    num_rows = len(A) 
+    num_rows = len(A)
 
     # Transform matrices A and B into the augmented matrix M
-    M = augmented_matrix(A,B)
-    
+    M = augmented_matrix(A, B)
+
     # Iterate over the rows.
     for row in range(num_rows):
 
-        # The first pivot candidate is always in the main diagonal. 
-        # The diagonal elements in a matrix has the same index for row and column. 
+        # The first pivot candidate is always in the main diagonal.
+        # The diagonal elements in a matrix has the same index for row and
+        # column.
         pivot_candidate = M[row, row]
 
-        # If pivot_candidate is zero, it cannot be a pivot for this row. 
-        # So the first step is to look at the rows below it to check if there is a non-zero element in the same column.
-        if np.isclose(pivot_candidate, 0) == True: 
-            first_non_zero_value_below_pivot_candidate = get_index_first_non_zero_value_from_column(M, row, row) 
+        # If pivot_candidate is zero, it cannot be a pivot for this row.
+        # So the first step is to look at the rows below it to check if there
+        # is a non-zero element in the same column.
+        if np.isclose(pivot_candidate, 0) == True:
+            first_non_zero_value_below_pivot_candidate = (
+                get_index_first_non_zero_value_from_column(M, row, row)
+            )
 
-            M = swap_rows(M, row, first_non_zero_value_below_pivot_candidate) 
+            M = swap_rows(M, row, first_non_zero_value_below_pivot_candidate)
 
-            pivot = M[row,row] 
-        
-        # If pivot_candidate is already non-zero, then it is the pivot for this row
+            pivot = M[row, row]
+
+        # If pivot_candidate is already non-zero, then it is the pivot for
+        # this row
         else:
-            pivot = pivot_candidate 
-        
+            pivot = pivot_candidate
+
         # Preform row reduction in every row below the current
-            
-        # Divide the current row by the pivot, so the new pivot will be 1. Use formula current_row -> 1/pivot * current_row
+
+        # Divide the current row by the pivot, so the new pivot will be 1. Use
+        # formula current_row -> 1/pivot * current_row
         # Where current_row can be accessed using M[row].
-        M[row] = (1/pivot) * M[row]
+        M[row] = (1 / pivot) * M[row]
 
         # Perform row reduction for rows below the current row
-        for j in range(row + 1, num_rows): 
-            # Get the value in the row that is below the pivot value. 
-            # The values in row j that are below the pivot must have column index the same index as the column index for the pivot.
-            value_below_pivot = M[j,row]
-            
+        for j in range(row + 1, num_rows):
+            # Get the value in the row that is below the pivot value.
+            # The values in row j that are below the pivot must have column
+            # index the same index as the column index for the pivot.
+            value_below_pivot = M[j, row]
+
             # Perform row reduction using the formula:
             # row_to_reduce -> row_to_reduce - value_below_pivot * pivot_row
-            M[j] = M[j] - value_below_pivot*M[row]
-   
+            M[j] = M[j] - value_below_pivot * M[row]
+
     return M
+
 
 def back_substitution(M):
     """
-    Perform back substitution on an augmented matrix (with unique solution) in reduced row echelon form to find the solution to the linear system.
+    Perform back substitution on an augmented matrix (with unique solution) in
+    reduced row echelon form to find the solution to the linear system.
 
     Parameters:
-    - M (numpy.array): The augmented matrix in row echelon form with unitary pivots (n x n+1).
+    - M (numpy.array): The augmented matrix in row echelon form with unitary
+        pivots (n x n+1).
 
     Returns:
     numpy.array: The solution vector of the linear system.
     """
-    
+
     # Make a copy of the input matrix to avoid modifying the original
     M = M.copy()
 
@@ -165,58 +186,67 @@ def back_substitution(M):
     num_rows = M.shape[0]
 
     # Iterate from bottom to top
-    for row in reversed(range(num_rows)): 
+    for row in reversed(range(num_rows)):
         substitution_row = M[row]
 
         # Get the index of the first non-zero element in the substitution row.
-        index = get_index_first_non_zero_value_from_row(M, row, augmented = True)
+        index = get_index_first_non_zero_value_from_row(M, row, augmented=True)
         # Iterate over the rows above the substitution_row
-        for j in reversed(range(row)): 
+        for j in reversed(range(row)):
             # Get the row to be reduced.
             row_to_reduce = M[j]
 
-            # Get the value of the element at the found index in the row to reduce
+            # Get the value of the element at the found index in the row to
+            # reduce
             value = row_to_reduce[index]
-            
-            # Perform the back substitution step using the formula row_to_reduce -> row_to_reduce - value * substitution_row
+
+            # Perform the back substitution step using the formula
+            # row_to_reduce -> row_to_reduce - value * substitution_row
             row_to_reduce = row_to_reduce - value * substitution_row
 
             # Replace the updated row in the matrix
-            M[j,:] = row_to_reduce
+            M[j, :] = row_to_reduce
 
-     # Extract the solution from the last column
-    solution = M[:,-1]
-    
+    # Extract the solution from the last column
+    solution = M[:, -1]
+
     return solution
+
 
 def gaussian_elimination(A, B):
     """
-    Solve a linear system represented by an augmented matrix using the Gaussian elimination method.
+    Solve a linear system represented by an augmented matrix using the
+    Gaussian elimination method.
 
     Parameters:
-    - A (numpy.array): Square matrix of size n x n representing the coefficients of the linear system
-    - B (numpy.array): Column matrix of size 1 x n representing the constant terms.
+    - A (numpy.array): Square matrix of size n x n representing the
+        coefficients of the linear system
+    - B (numpy.array): Column matrix of size 1 x n representing the constant
+        terms.
 
     Returns:
-    numpy.array or str: The solution vector if a unique solution exists, or a string indicating the type of solution.
+    numpy.array or str: The solution vector if a unique solution exists, or a
+        string indicating the type of solution.
     """
 
     # Get the matrix in row echelon form
     row_echelon_M = row_echelon_form(A, B)
 
-    # If the system is non-singular, then perform back substitution to get the result. 
+    # If the system is non-singular, then perform back substitution to get the
+    # result.
     # The function row_echelon_form returns a string if there is no solution
-    if not isinstance(row_echelon_M, str): 
+    if not isinstance(row_echelon_M, str):
         solution = back_substitution(row_echelon_M)
         return solution
     else:
-        return 'Singular system'
+        return "Singular system"
+
 
 def string_to_augmented_matrix(equations):
     """Reads in a string of equations and returns the augmented matrix.
 
     Args:
-        equations (_type_): like the following strings for equations 
+        equations (_type_): like the following strings for equations
             3*x + 6*y + 6*w + 8*z = 1
             5*x + 3*y + 6*w = -10
             4*y - 5*w + 8*z = 8
@@ -226,31 +256,32 @@ def string_to_augmented_matrix(equations):
         tuple: tuple of strings and numpy arrays
     """
     # Split the input string into individual equations
-    equation_list = equations.split('\n')
-    equation_list = [x for x in equation_list if x != '']
+    equation_list = equations.split("\n")
+    equation_list = [x for x in equation_list if x != ""]
     # Create a list to store the coefficients and constants
     coefficients = []
-    
-    ss = ''
+
+    ss = ""
     for c in equations:
-        if c in 'abcdefghijklmnopqrstuvwxyz':
+        if c in "abcdefghijklmnopqrstuvwxyz":
             if c not in ss:
-                ss += c + ' '
+                ss += c + " "
     ss = ss[:-1]
-    
+
     # Create symbols for variables x, y, z, etc.
     variables = symbols(ss)
     # Parse each equation and extract coefficients and constants
     for equation in equation_list:
         # Remove spaces and split into left and right sides
-        sides = equation.replace(' ', '').split('=')
-        
+        sides = equation.replace(" ", "").split("=")
+
         # Parse the left side using SymPy's parser
         left_side = sympify(sides[0])
-        
+
         # Extract coefficients for variables
-        coefficients.append([left_side.coeff(variable) for variable in variables])
-        
+        coefficients.append([left_side.coeff(variable) for variable in
+                             variables])
+
         # Append the constant term
         coefficients[-1].append(int(sides[1]))
 
@@ -258,6 +289,7 @@ def string_to_augmented_matrix(equations):
     augmented_matrix = Matrix(coefficients)
     augmented_matrix = np.array(augmented_matrix).astype("float64")
 
-    A, B = augmented_matrix[:,:-1], augmented_matrix[:,-1].reshape(-1,1)
-    
+    A, B = augmented_matrix[:, :-1], augmented_matrix[:, -1].reshape(-1, 1)
+
     return ss, A, B
+
